bed3e31634ba1c891a26334b197653440e8fb081
Magdalena Sułkowska <magdalena.sulkowska@icloud.com>
2019-10-28 02:55:10 +0100

    Updates

---
+++ precache-manifest.499dd14340a02f553e18b5832c69032d.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a5919e5b3e8eea7086f49c09a080180",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3137c72a95f6c84d55a0",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "e4f59379da646f7766cd",
+    "url": "/static/css/main.391ab913.chunk.css"
+  },
+  {
+    "revision": "3137c72a95f6c84d55a0",
+    "url": "/static/js/2.5bbe660e.chunk.js"
+  },
+  {
+    "revision": "e4f59379da646f7766cd",
+    "url": "/static/js/main.c73c446e.chunk.js"
+  },
+  {
+    "revision": "4b7cdb98ed15195f6d4b",
+    "url": "/static/js/runtime-main.32f341b7.js"
+  },
+  {
+    "revision": "2fb5557acd57270956f8f56ac27cac35",
+    "url": "/static/media/background_80.2fb5557a.PNG"
+  },
+  {
+    "revision": "bc192b0d1f37ee4718de19d1f145813f",
+    "url": "/static/media/beige_button_back.bc192b0d.PNG"
+  },
+  {
+    "revision": "09a46afb0a9853f0292b6660833f0751",
+    "url": "/static/media/beige_button_left.09a46afb.PNG"
+  },
+  {
+    "revision": "3a068bdebfe0ccdd68dd0c843a6498c2",
+    "url": "/static/media/beige_button_right.3a068bde.PNG"
+  },
+  {
+    "revision": "bb4cad51e9908c2812cf457745f5ab48",
+    "url": "/static/media/beige_button_shadow.bb4cad51.PNG"
+  },
+  {
+    "revision": "3c6704caad719eaa3b96f954f20fec59",
+    "url": "/static/media/blue_led_light.3c6704ca.PNG"
+  },
+  {
+    "revision": "e23974d240431685460344c9de3d70f7",
+    "url": "/static/media/cross_gray_down.e23974d2.PNG"
+  },
+  {
+    "revision": "127b66d68179a35f4a2b209cb6e50eea",
+    "url": "/static/media/cross_gray_down_shadow.127b66d6.PNG"
+  },
+  {
+    "revision": "c6021c9a0817aead7329b8f967ddd58f",
+    "url": "/static/media/cross_gray_left.c6021c9a.PNG"
+  },
+  {
+    "revision": "141a0fc4627b21c5c83a25665140d0ee",
+    "url": "/static/media/cross_gray_left_shadow.141a0fc4.PNG"
+  },
+  {
+    "revision": "214a941d93c6769042b29b695d0bbbbf",
+    "url": "/static/media/cross_gray_middle.214a941d.PNG"
+  },
+  {
+    "revision": "4e0dcdfc497b249377687bdb4004fbc0",
+    "url": "/static/media/cross_gray_right.4e0dcdfc.PNG"
+  },
+  {
+    "revision": "e5622f0d72617925dbe313f61069b5a0",
+    "url": "/static/media/cross_gray_right_shadow.e5622f0d.PNG"
+  },
+  {
+    "revision": "4afdb024cc9ac51506784f94c735efc0",
+    "url": "/static/media/cross_gray_up.4afdb024.PNG"
+  },
+  {
+    "revision": "dbbd4a20508973f83e16dca439ba9434",
+    "url": "/static/media/cross_gray_up_shadow.dbbd4a20.PNG"
+  },
+  {
+    "revision": "cb7087587039e190a2468e09429ac46b",
+    "url": "/static/media/green_screen_cross_2.cb708758.PNG"
+  },
+  {
+    "revision": "b8beea01a9d00a6d6756b859055542f9",
+    "url": "/static/media/pokedex_without_buttons.b8beea01.PNG"
+  },
+  {
+    "revision": "8b0eb5dd4bbb417f1bf4948612fa5a78",
+    "url": "/static/media/small_gray_button.8b0eb5dd.PNG"
+  },
+  {
+    "revision": "7c82abe8dcfec5e1771cb60d5d17f6a7",
+    "url": "/static/media/small_gray_button_shadow.7c82abe8.PNG"
+  },
+  {
+    "revision": "56657b00b589ec5505175aa4298e1f2a",
+    "url": "/static/media/yellow_led_light.56657b00.PNG"
+  }
+]);
