2d739a07c4adef6a4856015f9616dcb2d4ed0ecc
Magdalena Sułkowska <magdalena.sulkowska@icloud.com>
2019-12-17 21:01:25 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5ea1156dbd831ddb66dbd72e661ed310.js"
+  "/precache-manifest.a8d8b6249d2377d8614ba0f0d03d2299.js"
 );
 
 self.addEventListener('message', (event) => {
